d5c4980563ac16aed2dd80ac5ae749276e73ce58
Zygrade <hsenday.6991@gmail.com>
2019-01-20 16:20:15 +0530

    touch up

---
+++ public/precache-manifest.4e75b44d8bd7a7386e22b219885d6f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "735a734e60cdaaa5428f",
+    "url": "/static/css/main.e304ad7f.chunk.css"
+  },
+  {
+    "revision": "735a734e60cdaaa5428f",
+    "url": "/static/js/main.735a734e.chunk.js"
+  },
+  {
+    "revision": "b70564b3999037336294",
+    "url": "/static/js/1.b70564b3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60bb4c778dd5dd5ecaf7842f4ca3fe2e",
+    "url": "/index.html"
+  }
+];
