f236d686affb5b6236f094845c3741a79ceb677c
Zygrade <hsenday.6991@gmail.com>
2019-01-20 13:43:18 +0530

    react && apollo init

---
+++ client/src/components/Launches.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import LaunchItem from './LaunchItem'
+
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery{
+    launches {
+      flight_number,
+      launch_success,
+      mission_name,
+      launch_year,
+      launch_date_local
+    }
+  }    
+`;
+
+export class Launches extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1 className="display-4 my-3">Launches</h1>
+        <Query query={LAUNCHES_QUERY}>
+        {
+            ({loading,data,error}) => {
+
+                if(loading) return <h4>Loading...</h4>;
+
+                if(error) console.log(error);
+
+                //console.log(data);
+
+                return <Fragment>
+                    {
+                        data.launches.map(launch => (
+                            <LaunchItem key={launch.flight_number} launch={launch}></LaunchItem>
+                        ))
+                    }
+                </Fragment>   ; 
+            }
+        }
+        </Query>
+      </Fragment>
+    )
+  }
+}
+
+export default Launches
